259ed03d09e4c1644c8b58e163e1883643071a78
Akhil Teli <akhilteli55@gmail.com>
2021-08-25 01:53:08 +0530

    created login and sign up page

--- app/src/main/java/com/example/myapplication/page3.java
+++ app/src/main/java/com/example/myapplication/page3.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class page3 extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class page3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page3);
+        ImageView n =findViewById(R.id.btRegister1);
+        n.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ety = new Intent(page3.this, login.class);
+                //intent.putExtra("EXTRA_SESSION_ID", count);
+                startActivity(ety);
+            }
+        });
     }
-}
+    }
